309da8368d7af47db55a277fe5b2131c4a374a86
Komilkhon <temurkhanov.akfa@gmail.com>
2024-05-04 23:25:28 +0500

    ReadIntFromConsole()

--- code/Program.cs
+++ code/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadIntFromConsole()
+{
+    int target_data;
+data_input:
+    Console.WriteLine("Input string array length:");
+    target_data = Convert.ToInt32(Console.ReadLine());
+    if (target_data == 0)
+    {
+        Console.WriteLine("Length of input string must differ from 0");
+        goto data_input;
+    }
+    return target_data;
+}
