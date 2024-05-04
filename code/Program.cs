010ee4ace1ba8959f82b439a0a096234b2f47c03
Komilkhon <temurkhanov.akfa@gmail.com>
2024-05-04 23:29:16 +0500

    added initialization of main code and the code to fill and check input_string

--- code/Program.cs
+++ code/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int input_data_size = 0;
+int match_count = 0;
+int required_string_length = 3;
+input_data_size = ReadIntFromConsole();
+Console.WriteLine(input_data_size);
+bool[] match_string_adress = new bool[input_data_size];
+string[] input_data = new string[input_data_size];
+for (int i = 0; i < input_data_size; i++)
+{
+    input_data[i] = ReadStringFromConsole();
+    if (CheckIfMatch(input_data[i], required_string_length))
+    {
+        match_string_adress[i] = true;
+        match_count++;
+    }
+    else
+    {
+        match_string_adress[i] = false;
+    }
+}
