d7bca99b3a7b8a802e34116bd15b52a813c4f027
Komilkhon <temurkhanov.akfa@gmail.com>
2024-05-04 23:26:19 +0500

    ReadStringFromConsole()

--- code/Program.cs
+++ code/Program.cs
@@ -11,3 +11,19 @@ data_input:
     }
     return target_data;
 }
+string ReadStringFromConsole()
+{
+    string input_string = "0";
+data_input:
+    Console.WriteLine("Input some characters:");
+    input_string = Console.ReadLine();
+    if (input_string.Length == 0)
+    {
+        Console.WriteLine("Length of input string must differ from 0");
+        goto data_input;
+    }
+    else
+    {
+        return input_string;
+    }
+}
